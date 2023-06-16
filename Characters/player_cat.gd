79bf1bee092ace88a32840e49bcccb1eaa9b8001
sonukuldeep <sonukuldeep@outlook.com>
2023-06-16 17:29:40 +0530

    init

---
+++ Characters/player_cat.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 100
+
+func _physics_process(_delta):
+#	get input direction
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	#print(input_direction)
+	
+	velocity = input_direction * move_speed
+	
+	move_and_slide()
